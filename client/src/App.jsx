32cdbf29fb49b523384dc99fda095a2b43d60087
sauummm <126983204+sauummm@users.noreply.github.com>
2025-05-06 23:35:03 +0530

    more work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Layout from './Layout.jsx';
 import RegisterPage from './Pages/RegisterPage.jsx';
 import axios from 'axios';
 import { UserContextProvider } from './UserContext.jsx';
+import AccountPage from './Pages/AccountPage.jsx';
+import {Link,Navigate} from 'react-router-dom'; 
 
 axios.defaults.baseURL = 'http://localhost:4000';
 
@@ -17,6 +19,8 @@ function App() {
         <Route index element={<IndexPage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/account/:subpage?' element= {<AccountPage />} />
+
       
       </Route>
       
--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -7,11 +7,13 @@ export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
+    const [ready, setReady] = useState(false);
     useEffect( () => {
         if(!user){
             const {data} = axios.get('/profile', {withCredentials:true})
             .then((data) => {
                 setUser(data);
+                setReady(true);
 
             })
             
@@ -19,7 +21,7 @@ export function UserContextProvider({ children }) {
     }, [])
   
     return (
-      <UserContext.Provider value={{ user, setUser }}>
+      <UserContext.Provider value={{ user, setUser, ready }}>
         {children}
       </UserContext.Provider>
     );
